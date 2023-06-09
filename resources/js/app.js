b8f8b09b45b24749e4ac9ea19f8bc3df9f77ed92
richar-iqfq <ricardoambmed@gmail.com>
2023-06-09 16:47:39 -0600

    first_instance of Vue

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import Home from './components/Home.vue';
+
+const app = createApp({});
+
+app.component('home-component', Home);
+
+app.mount("#app");
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    How to Add Vue js 3 to Laravel 10 with Vite
+</template>
