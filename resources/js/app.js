b3f7d045256bca9e2b36617abd54180f08e9c19e
richar-iqfq <ricardoambmed@gmail.com>
2023-07-21 18:42:26 -0600

    Independent installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,30 @@
 // import {createApp} from 'vue/dist/vue.esm-bundler.js'; 
 import {createApp} from 'vue'; // instead of line 1 thanks to resolve:alias in vite.config.js
 
-import home from './home.vue'; // home component
 import '../css/app.css'; // import styles here instead of vite for efficiency
 
-const app = createApp({})
-    .component('home-component', home)
-    .mount("#app");
+const app = createApp({
+    data()
+        {
+            return {
+                message : 'Using Vue.js 3 on Laravel',
+                value : 0
+            }
+        },
+        methods : 
+        {
+            increase() {
+                this.value ++
+            },
+            decrease() {
+                this.value --
+            }
+
+        },
+        computed: 
+        {
+            isButtonDisable() {
+                return this.value == 0 ? true : false
+            }
+        }
+}).mount("#app");
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,14 @@
 
 @section('content')
     @vite('resources/js/app.js')
+    @verbatim
+    
     <main id="app">
-        <home-component></home-component>
+        <br>{{message}}<br>
+        El valor es: {{value}}<br><br>
+        <button @click="increase">Incrementar</button>
+        <button @click="decrease" :disabled="isButtonDisable">Disminuir</button>
     </main>
+
+    @endverbatim
 @endsection
